60cbf518184ed9796600f4f25e0144fdb2b1deee
Timoteo Rider <timoteo.rider@ctpcit.ed.cr>
2022-09-27 15:10:25 -0600

    Add base model auditable

--- ExpoCIT/ExpoContext.cs
+++ ExpoCIT/ExpoContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using ExpoCIT.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ExpoCIT
 {
@@ -6,6 +7,33 @@ namespace ExpoCIT
     {
         public ExpoContext(DbContextOptions<ExpoContext> options) : base (options) { }
 
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            var instertedEntries = this.ChangeTracker.Entries()
+                .Where(x => x.State == EntityState.Added)
+                .Select(x => x.Entity);
 
+            foreach (var insertedEntry in instertedEntries)
+            {
+                var auditableEntity = insertedEntry as Auditable;
+
+                if (auditableEntity != null)
+                    auditableEntity.FechaCreado = DateTime.Now;
+            }
+
+            var modifiedEntries = this.ChangeTracker.Entries()
+                .Where(x => x.State == EntityState.Modified)
+                .Select(x => x.Entity);
+
+            foreach (var modifiedEntry in modifiedEntries)
+            {
+                var auditableEntity = modifiedEntry as Auditable;
+
+                if (auditableEntity != null)
+                    auditableEntity.FechaModificado = DateTime.Now;
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
     }
 }
---
+++ ExpoCIT/Models/Auditable.cs
@@ -0,0 +1,8 @@
+﻿namespace ExpoCIT.Models
+{
+    public abstract class Auditable
+    {
+        public DateTime FechaCreado { get; set; }
+        public DateTime? FechaModificado { get; set; }
+    }
+}
