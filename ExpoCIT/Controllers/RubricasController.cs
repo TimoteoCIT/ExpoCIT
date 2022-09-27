26b0bea5484e28acd33c21324dbf6802ec261e74
Andres Bolannos <andresbol1706@gmail.com>
2022-09-27 14:36:04 -0600

    Controladores y Views

---
+++ ExpoCIT/Controllers/RubricasController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExpoCIT.Controllers
+{
+    public class RubricasController : Controller
+    {
+        public IActionResult FormProyecto()
+        {
+            return View();
+        }
+
+        public IActionResult FormTrabajoEscrito()
+        {
+            return View();
+        }
+        public IActionResult RubricaProyecto()
+        {
+            return View();
+        }
+        public IActionResult RubricaTrabajoEscrito()
+        {
+            return View();
+        }
+    }
+}
