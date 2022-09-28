d56f3f388215576ae541050284f79d4963446b3c
Timoteo Rider <Timoteo2.25@gmail.com>
2022-09-27 21:46:14 -0600

    Add Usuario model

---
+++ ExpoCIT/Models/Usuario.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace ExpoCIT.Models
+{
+    public class Usuario : Auditable
+    {
+        [Required(ErrorMessage = "Porfavor ingresar el username.")]
+        string Username { get; set; }
+
+        [Required(ErrorMessage = "Porfavor ingresar la contraseña.")]
+        [Display(Name = "Contraseña")]
+        [DataType(DataType.Password)]
+        string Contrasena { get; set; }
+
+        string Nombre { get; set; }
+
+        [Display(Name = "Primer Apellido")]
+        string PrimerApellido { get; set; }
+
+        [Display(Name = "Segundo Apellido")]
+        string SegundoApellido { get; set; }
+    }
+}
