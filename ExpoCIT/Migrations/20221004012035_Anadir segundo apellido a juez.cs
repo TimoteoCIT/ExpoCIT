a4588210ba3bb65a1a8aec7794dce90e6bab3ca9
Timoteo Rider <Timoteo2.25@gmail.com>
2022-10-03 19:21:01 -0600

    Hacer segundo apellido de juez publico

---
+++ ExpoCIT/Migrations/20221004012035_Anadir segundo apellido a juez.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExpoCIT.Migrations
+{
+    public partial class Anadirsegundoapellidoajuez : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SegundoApellido",
+                table: "Jueces",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SegundoApellido",
+                table: "Jueces");
+        }
+    }
+}
