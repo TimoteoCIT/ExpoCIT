d82aab4dd644fff21cc85e2c8e2f5d4f7ba43b74
Timoteo Rider <Timoteo2.25@gmail.com>
2022-10-06 22:50:29 -0600

    Connect RPEI view to database

--- ExpoCIT/Controllers/JuezController.cs
+++ ExpoCIT/Controllers/JuezController.cs
@@ -54,17 +54,35 @@ namespace ExpoCIT.Controllers
         }
 
         [HttpPost]
-        public IActionResult FormProyectoExpoIngenieria(RPEI rpei)
+        public IActionResult FormProyectoExpoIngenieria(RPEI rpei, int idProyecto, string firma)
         {
-            RPEI? dbRPEI = _db.RPEIs.Find(rpei.Id);
+            var dbRPEI = _db.RPEIs.Include(x => x.Proyecto).FirstOrDefault(x => x.Proyecto.Id == idProyecto);
+            dbRPEI ??= new RPEI();
+            rpei.Proyecto = _db.Proyectos.Find(idProyecto) ?? throw new InvalidOperationException("Deberia ser imposible que el id del proyecto no exista");
 
-            if (dbRPEI == null)
-                return View();
+            rpei.I_subtotal = rpei.I_a + rpei.I_b + rpei.I_c + rpei.I_d + rpei.I_e;
+            rpei.II_subtotal = rpei.II_a + rpei.II_b + rpei.II_c + rpei.II_d + rpei.II_e + rpei.II_f;
+            rpei.III_subtotal = rpei.III_a + rpei.III_b + rpei.III_c + rpei.III_d + rpei.III_e;
+            rpei.IV_subtotal = rpei.IV_a + rpei.IV_b + rpei.IV_c + rpei.IV_d + rpei.IV_e + rpei.IV_f;
+            rpei.V_subtotal = rpei.V_a + rpei.V_b + rpei.V_c + rpei.V_d + rpei.V_e + rpei.V_f;
+            rpei.VI_subtotal = rpei.VI_a + rpei.VI_b + rpei.VI_c + rpei.VI_d + rpei.VI_e + rpei.VI_f + rpei.VI_g + rpei.VI_h;
+            rpei.VII_subtotal = rpei.VII_a + rpei.VII_b + rpei.VII_c + rpei.VII_d + rpei.VII_e + rpei.VII_f;
+            rpei.VIII_subtotal = rpei.VIII_a + rpei.VIII_b + rpei.VIII_c + rpei.VIII_d;
+
+            rpei.estado = true;
+
+            var encodedImage = firma.Split(',')[1];
+            var decodedImage = Convert.FromBase64String(encodedImage);
+            rpei.FirmaDigital = decodedImage;
 
             dbRPEI = rpei;
+            if (dbRPEI.Id == 0)
+            {
+                _db.RPEIs.Add(dbRPEI);
+            }
 
             _db.SaveChanges();
-            return View("Index");
+            return RedirectToAction("Index");
         }
 
         public IActionResult FormTrabajoEscritoExpoIngenieria(int idProyecto)
