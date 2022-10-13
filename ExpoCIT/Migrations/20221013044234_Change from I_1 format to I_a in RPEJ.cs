b30df3fe30f902aeb2a8285c3fe1050da54ce640
Timoteo Rider <Timoteo2.25@gmail.com>
2022-10-12 22:46:46 -0600

    Update RPEJ to be inline with other models

---
+++ ExpoCIT/Migrations/20221013044234_Change from I_1 format to I_a in RPEJ.cs
@@ -0,0 +1,365 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExpoCIT.Migrations
+{
+    public partial class ChangefromI_1formattoI_ainRPEJ : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "V_27",
+                table: "RTEJs",
+                newName: "V_e");
+
+            migrationBuilder.RenameColumn(
+                name: "V_26",
+                table: "RTEJs",
+                newName: "V_d");
+
+            migrationBuilder.RenameColumn(
+                name: "V_25",
+                table: "RTEJs",
+                newName: "V_c");
+
+            migrationBuilder.RenameColumn(
+                name: "V_24",
+                table: "RTEJs",
+                newName: "V_b");
+
+            migrationBuilder.RenameColumn(
+                name: "V_23",
+                table: "RTEJs",
+                newName: "V_a");
+
+            migrationBuilder.RenameColumn(
+                name: "VI_31",
+                table: "RTEJs",
+                newName: "VI_d");
+
+            migrationBuilder.RenameColumn(
+                name: "VI_30",
+                table: "RTEJs",
+                newName: "VI_c");
+
+            migrationBuilder.RenameColumn(
+                name: "VI_29",
+                table: "RTEJs",
+                newName: "VI_b");
+
+            migrationBuilder.RenameColumn(
+                name: "VI_28",
+                table: "RTEJs",
+                newName: "VI_a");
+
+            migrationBuilder.RenameColumn(
+                name: "VII_35",
+                table: "RTEJs",
+                newName: "VII_d");
+
+            migrationBuilder.RenameColumn(
+                name: "VII_34",
+                table: "RTEJs",
+                newName: "VII_c");
+
+            migrationBuilder.RenameColumn(
+                name: "VII_33",
+                table: "RTEJs",
+                newName: "VII_b");
+
+            migrationBuilder.RenameColumn(
+                name: "VII_32",
+                table: "RTEJs",
+                newName: "VII_a");
+
+            migrationBuilder.RenameColumn(
+                name: "I_5",
+                table: "RTEJs",
+                newName: "I_e");
+
+            migrationBuilder.RenameColumn(
+                name: "I_4",
+                table: "RTEJs",
+                newName: "I_d");
+
+            migrationBuilder.RenameColumn(
+                name: "I_3",
+                table: "RTEJs",
+                newName: "I_c");
+
+            migrationBuilder.RenameColumn(
+                name: "I_2",
+                table: "RTEJs",
+                newName: "I_b");
+
+            migrationBuilder.RenameColumn(
+                name: "I_1",
+                table: "RTEJs",
+                newName: "I_a");
+
+            migrationBuilder.RenameColumn(
+                name: "IV_22",
+                table: "RTEJs",
+                newName: "IV_f");
+
+            migrationBuilder.RenameColumn(
+                name: "IV_21",
+                table: "RTEJs",
+                newName: "IV_e");
+
+            migrationBuilder.RenameColumn(
+                name: "IV_20",
+                table: "RTEJs",
+                newName: "IV_d");
+
+            migrationBuilder.RenameColumn(
+                name: "IV_19",
+                table: "RTEJs",
+                newName: "IV_c");
+
+            migrationBuilder.RenameColumn(
+                name: "IV_18",
+                table: "RTEJs",
+                newName: "IV_b");
+
+            migrationBuilder.RenameColumn(
+                name: "IV_17",
+                table: "RTEJs",
+                newName: "IV_a");
+
+            migrationBuilder.RenameColumn(
+                name: "II_9",
+                table: "RTEJs",
+                newName: "II_f");
+
+            migrationBuilder.RenameColumn(
+                name: "II_8",
+                table: "RTEJs",
+                newName: "II_e");
+
+            migrationBuilder.RenameColumn(
+                name: "II_7",
+                table: "RTEJs",
+                newName: "II_d");
+
+            migrationBuilder.RenameColumn(
+                name: "II_6",
+                table: "RTEJs",
+                newName: "II_c");
+
+            migrationBuilder.RenameColumn(
+                name: "II_11",
+                table: "RTEJs",
+                newName: "II_b");
+
+            migrationBuilder.RenameColumn(
+                name: "II_10",
+                table: "RTEJs",
+                newName: "II_a");
+
+            migrationBuilder.RenameColumn(
+                name: "III_16",
+                table: "RTEJs",
+                newName: "III_e");
+
+            migrationBuilder.RenameColumn(
+                name: "III_15",
+                table: "RTEJs",
+                newName: "III_d");
+
+            migrationBuilder.RenameColumn(
+                name: "III_14",
+                table: "RTEJs",
+                newName: "III_c");
+
+            migrationBuilder.RenameColumn(
+                name: "III_13",
+                table: "RTEJs",
+                newName: "III_b");
+
+            migrationBuilder.RenameColumn(
+                name: "III_12",
+                table: "RTEJs",
+                newName: "III_a");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "V_e",
+                table: "RTEJs",
+                newName: "V_27");
+
+            migrationBuilder.RenameColumn(
+                name: "V_d",
+                table: "RTEJs",
+                newName: "V_26");
+
+            migrationBuilder.RenameColumn(
+                name: "V_c",
+                table: "RTEJs",
+                newName: "V_25");
+
+            migrationBuilder.RenameColumn(
+                name: "V_b",
+                table: "RTEJs",
+                newName: "V_24");
+
+            migrationBuilder.RenameColumn(
+                name: "V_a",
+                table: "RTEJs",
+                newName: "V_23");
+
+            migrationBuilder.RenameColumn(
+                name: "VI_d",
+                table: "RTEJs",
+                newName: "VI_31");
+
+            migrationBuilder.RenameColumn(
+                name: "VI_c",
+                table: "RTEJs",
+                newName: "VI_30");
+
+            migrationBuilder.RenameColumn(
+                name: "VI_b",
+                table: "RTEJs",
+                newName: "VI_29");
+
+            migrationBuilder.RenameColumn(
+                name: "VI_a",
+                table: "RTEJs",
+                newName: "VI_28");
+
+            migrationBuilder.RenameColumn(
+                name: "VII_d",
+                table: "RTEJs",
+                newName: "VII_35");
+
+            migrationBuilder.RenameColumn(
+                name: "VII_c",
+                table: "RTEJs",
+                newName: "VII_34");
+
+            migrationBuilder.RenameColumn(
+                name: "VII_b",
+                table: "RTEJs",
+                newName: "VII_33");
+
+            migrationBuilder.RenameColumn(
+                name: "VII_a",
+                table: "RTEJs",
+                newName: "VII_32");
+
+            migrationBuilder.RenameColumn(
+                name: "I_e",
+                table: "RTEJs",
+                newName: "I_5");
+
+            migrationBuilder.RenameColumn(
+                name: "I_d",
+                table: "RTEJs",
+                newName: "I_4");
+
+            migrationBuilder.RenameColumn(
+                name: "I_c",
+                table: "RTEJs",
+                newName: "I_3");
+
+            migrationBuilder.RenameColumn(
+                name: "I_b",
+                table: "RTEJs",
+                newName: "I_2");
+
+            migrationBuilder.RenameColumn(
+                name: "I_a",
+                table: "RTEJs",
+                newName: "I_1");
+
+            migrationBuilder.RenameColumn(
+                name: "IV_f",
+                table: "RTEJs",
+                newName: "IV_22");
+
+            migrationBuilder.RenameColumn(
+                name: "IV_e",
+                table: "RTEJs",
+                newName: "IV_21");
+
+            migrationBuilder.RenameColumn(
+                name: "IV_d",
+                table: "RTEJs",
+                newName: "IV_20");
+
+            migrationBuilder.RenameColumn(
+                name: "IV_c",
+                table: "RTEJs",
+                newName: "IV_19");
+
+            migrationBuilder.RenameColumn(
+                name: "IV_b",
+                table: "RTEJs",
+                newName: "IV_18");
+
+            migrationBuilder.RenameColumn(
+                name: "IV_a",
+                table: "RTEJs",
+                newName: "IV_17");
+
+            migrationBuilder.RenameColumn(
+                name: "II_f",
+                table: "RTEJs",
+                newName: "II_9");
+
+            migrationBuilder.RenameColumn(
+                name: "II_e",
+                table: "RTEJs",
+                newName: "II_8");
+
+            migrationBuilder.RenameColumn(
+                name: "II_d",
+                table: "RTEJs",
+                newName: "II_7");
+
+            migrationBuilder.RenameColumn(
+                name: "II_c",
+                table: "RTEJs",
+                newName: "II_6");
+
+            migrationBuilder.RenameColumn(
+                name: "II_b",
+                table: "RTEJs",
+                newName: "II_11");
+
+            migrationBuilder.RenameColumn(
+                name: "II_a",
+                table: "RTEJs",
+                newName: "II_10");
+
+            migrationBuilder.RenameColumn(
+                name: "III_e",
+                table: "RTEJs",
+                newName: "III_16");
+
+            migrationBuilder.RenameColumn(
+                name: "III_d",
+                table: "RTEJs",
+                newName: "III_15");
+
+            migrationBuilder.RenameColumn(
+                name: "III_c",
+                table: "RTEJs",
+                newName: "III_14");
+
+            migrationBuilder.RenameColumn(
+                name: "III_b",
+                table: "RTEJs",
+                newName: "III_13");
+
+            migrationBuilder.RenameColumn(
+                name: "III_a",
+                table: "RTEJs",
+                newName: "III_12");
+        }
+    }
+}
--- ExpoCIT/Models/RPEJ.cs
+++ ExpoCIT/Models/RPEJ.cs
@@ -4,53 +4,53 @@
     {
         public int Id { get; set; }
 
-        public int I_1 { get; set; }
-        public int I_2 { get; set; }
-        public int I_3 { get; set; }
-        public int I_4 { get; set; }
-        public int I_5 { get; set; }
+        public int I_a { get; set; }
+        public int I_b { get; set; }
+        public int I_c { get; set; }
+        public int I_d { get; set; }
+        public int I_e { get; set; }
         public int I_subtotal { get; set; }
 
-        public int II_6 { get; set; }
-        public int II_7 { get; set; }
-        public int II_8 { get; set; }
-        public int II_9 { get; set; }
-        public int II_10 { get; set; }
-        public int II_11 { get; set; }
+        public int II_a { get; set; }
+        public int II_b { get; set; }
+        public int II_c { get; set; }
+        public int II_d { get; set; }
+        public int II_e { get; set; }
+        public int II_f { get; set; }
         public int II_subtotal { get; set; }
 
-        public int III_12 { get; set; }
-        public int III_13 { get; set; }
-        public int III_14 { get; set; }
-        public int III_15 { get; set; }
-        public int III_16 { get; set; }
+        public int III_a { get; set; }
+        public int III_b { get; set; }
+        public int III_c { get; set; }
+        public int III_d { get; set; }
+        public int III_e { get; set; }
         public int III_subtotal { get; set; }
 
-        public int IV_17 { get; set; }
-        public int IV_18 { get; set; }
-        public int IV_19 { get; set; }
-        public int IV_20 { get; set; }
-        public int IV_21 { get; set; }
-        public int IV_22 { get; set; }
+        public int IV_a { get; set; }
+        public int IV_b { get; set; }
+        public int IV_c { get; set; }
+        public int IV_d { get; set; }
+        public int IV_e { get; set; }
+        public int IV_f { get; set; }
         public int IV_subtotal { get; set; }
 
-        public int V_23 { get; set; }
-        public int V_24 { get; set; }
-        public int V_25 { get; set; }
-        public int V_26 { get; set; }
-        public int V_27 { get; set; }
+        public int V_a { get; set; }
+        public int V_b { get; set; }
+        public int V_c { get; set; }
+        public int V_d { get; set; }
+        public int V_e { get; set; }
         public int V_subtotal { get; set; }
 
-        public int VI_28 { get; set; }
-        public int VI_29 { get; set; }
-        public int VI_30 { get; set; }
-        public int VI_31 { get; set; }
+        public int VI_a { get; set; }
+        public int VI_b { get; set; }
+        public int VI_c { get; set; }
+        public int VI_d { get; set; }
         public int VI_subtotal { get; set; }
 
-        public int VII_32 { get; set; }
-        public int VII_33 { get; set; }
-        public int VII_34 { get; set; }
-        public int VII_35 { get; set; }
+        public int VII_a { get; set; }
+        public int VII_b { get; set; }
+        public int VII_c { get; set; }
+        public int VII_d { get; set; }
         public int VII_subtotal { get; set; }
 
         public double Total
