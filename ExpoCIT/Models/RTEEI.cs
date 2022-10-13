09864ca209c300932e3542b977aaaa513f8c97d9
Timoteo Rider <Timoteo2.25@gmail.com>
2022-10-13 17:11:29 -0600

    Load signature from database

--- ExpoCIT/Models/RTEEI.cs
+++ ExpoCIT/Models/RTEEI.cs
@@ -130,7 +130,7 @@ namespace ExpoCIT.Models
 
         public string? DG { get; set; }
 
-        public byte[] FirmaDigital { get; set; }
+        public byte[] FirmaDigital { get; set; } = { };
 
         public int ProyectoId { get; set; }
         public Proyecto Proyecto { get; set; }
